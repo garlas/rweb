e1c065c93d6dec78a2f6083ef53d52e053faf74c
garin <garinnugroho1345@gmail.com>
2024-08-31 15:13:25 +0700

    tes animasi

--- main.js
+++ main.js
@@ -13,3 +13,18 @@ document.addEventListener("click", (event) => {
     navLinks.classList.remove("active");
   }
 });
+
+// animasi
+document.addEventListener("DOMContentLoaded", function () {
+  var tentangKami = document.querySelector(".tentang-kami");
+
+  function showTentangKami() {
+    if (tentangKami.getBoundingClientRect().top < window.innerHeight) {
+      tentangKami.classList.add("visible");
+      window.removeEventListener("scroll", showTentangKami);
+    }
+  }
+
+  window.addEventListener("scroll", showTentangKami);
+  showTentangKami(); // Check immediately if the section is in view
+});
