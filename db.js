6c71561ed1fa987ba9e4e32af84b364d4fbfd54b
garin <garinnugroho1345@gmail.com>
2024-09-03 20:19:57 +0700

    z

--- db.js
+++ db.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    // Gantilah `YOUR_MONGODB_CONNECTION_STRING` dengan string koneksi yang Anda salin dari MongoDB Atlas
     await mongoose.connect(
       "mongodb+srv://garinnugroho1345:wGmiQtrjTMm5Us8t@cluster0.panko.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     );
